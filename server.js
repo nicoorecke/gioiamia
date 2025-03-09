34587a6d367468a703927d4a457db99d0d5d2b1f
Nico <recke.nico@gmail.com>
2025-03-09 02:42:49 -0300

    Otro commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = 5000; // Puedes cambiar el puerto si es necesario
+
+// Cargar productos desde un JSON
+app.get("/productos", (req, res) => {
+    fs.readFile("productos.json", "utf8", (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: "Error al leer el archivo" });
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando correctamente");
+});
+``
+
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
