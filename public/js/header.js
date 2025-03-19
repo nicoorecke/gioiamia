28131d4b5c0b3db0df9d0d006f7000e821989745
Nico <recke.nico@gmail.com>
2025-03-19 02:18:49 -0300

    todos los accesos bien

--- public/js/header.js
+++ public/js/header.js
@@ -9,16 +9,6 @@ class HeaderComponent extends HTMLElement {
           <nav>
             <ul class="menu">
               <li><a href="/">Inicio</a></li>
-              <li class="submenu">
-                <a href="#" onclick="return false;">Catálogo</a>
-                <ul class="dropdown">
-                  <li><a href="menu-postres">Postres</a></li>
-                  <li><a href="menu-box">Box Regalo</a></li>
-                  <li><a href="menu-bandejas">Bandejitas</a></li>
-                  <li><a href="menu-desayunos">Desayunos</a></li>
-                  <li><a href="menu-picadas">Picadas</a></li>
-                  <li><a href="menu-congelados">Congelados</a></li>
-                </ul>
               </li>
               <li><a href="catering">Catering</a></li>
               <li><a href="contacto">Contacto</a></li>
